996b87614a000875786bc5736d2b1892de9ed69f
Shagun Agrawal <agrawalshagun07@gmail.com>
2022-11-27 22:01:34 +0530

    initial commit

---
+++ hello_world.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef struct {
+    float x;
+    float y;
+} point;
+
+int add_together(int x, int y) {
+    int res = x + y;
+    return res;
+}
+
+int main(int argc, char** argv) {
+    puts("Hello, world!");
+    return 0;
+}
